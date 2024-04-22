547c0198effe92c0caea965b752781319cd14282
YongLeeCode <yonghwan311@gmail.com>
2024-04-21 21:34:38 -0600

    second

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Home() {
+	return (
+		<div>
+      <h1>Yong Lee(이용환) | 24시간이부족한개발자</h1>
+      <hr />
+      <ul>
+        <li>React를활용한컴포넌트로구성된개인토이프로젝트와상용화프로젝트를개발합니다.</li>
+        <li>웹개발시반응형과웹표준,웹접근성을준수합니다.</li>
+        <li>cafe24,AWS(EC2,S3)를활용하여실버배포한경험이있습니다</li>
+        <li>개발블로그를통해코드공유및스터디를지속합니다.</li>
+      </ul>
+      <hr />
+      <div>
+        <div>
+          <h4>Contact</h4>
+          <p>Email: yong.lee.dev@gmail.com</p>
+          <p>Phone: 208.607.1656</p>
+        </div>
+        <div>
+          <h4>Channel</h4>
+          <p>GitHub: </p>
+          <p>Portfolio site: </p>
+        </div>
+      </div>
+      <hr />
+      <h3>Skills</h3>
+		</div>
+	);
+}
+
